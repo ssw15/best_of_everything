9d1cc25ea2d5dae1b7daa98724e71ab142beac55
Raghu Betina <rbetina@github.com>
2015-01-28 10:31:06 -0600

    Adds model logic

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,9 @@
 class Favorite < ActiveRecord::Base
+  validates :user, :presence => true
+  validates :dish, :presence => true
+  validates :venue, :presence => true
+
+  belongs_to :user
+  belongs_to :dish
+  belongs_to :venue
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,7 @@
 class Neighborhood < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => { :scope => :city }
+  validates :city, :presence => true
+
+  has_many :venues
+  has_many :favorites, :through => :venues
 end
